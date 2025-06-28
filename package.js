837d004b6f893a99dddf5ba091e72e775613f4d7
ogxcutex <cutexmods@gmail.com>
2025-06-28 11:05:13 +0530

    Create package.js

---
+++ package.js
@@ -0,0 +1,19 @@
+{
+  "name": "modsentinel-bot",
+  "version": "1.0.0",
+  "description": "Modern Telegram group moderation bot",
+  "main": "bot.js",
+  "scripts": {
+    "start": "node bot.js",
+    "dev": "nodemon bot.js"
+  },
+  "dependencies": {
+    "telegraf": "^4.16.3"
+  },
+  "devDependencies": {
+    "nodemon": "^3.0.2"
+  },
+  "keywords": ["telegram", "bot", "moderation", "spam"],
+  "author": "Cutex",
+  "license": "MIT"
+}
